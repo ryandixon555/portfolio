bedda53c9636791f7c5a4e4a19f0ebb5750167a9
Ryan Dixon <r.dixon@reply.com>
2020-01-23 17:29:05 +0000

    added projects

--- source/js/projects.js
+++ source/js/projects.js
@@ -1,6 +1,30 @@
 module.exports = exports = [
 	{
-		id: 10,
+		id: 6,
+		client: "Personal Projects",
+		name: "Recipe App",
+		description: "An app showing you recipes. Made using REactJS where is connects to an API. Also includes filtering.",
+		tags: ["ReactJS", "filtering", "JavaScript"],
+		notes: [],
+		contentFolder: "react recipe",
+		noImages: 1,
+		playableLink: "https://react-js-recipe.netlify.com/",
+		githubLink: ""
+	},
+	{
+		id: 5,
+		client: "Personal Projects",
+		name: "To Do List",
+		description: "A To Do list made using ReactJS",
+		tags: ["ReactJS", "JavaScript"],
+		notes: [],
+		contentFolder: "react to do",
+		noImages: 2,
+		playableLink: "https://react-js-to-do.netlify.com/",
+		githubLink: "https://github.com/ryandixon555/react-todo-app"
+	},
+	{
+		id: 4,
 		client: "Avvio",
 		name: "Wordsearch Game",
 		description: "A wordsearch game",
@@ -12,7 +36,7 @@ module.exports = exports = [
 		githubLink: "https://github.com/ryandixon555/avvio-wordsearch/tree/master"
 	},
 	{
-		id: 9,
+		id: 3,
 		client: "Avvio",
 		name: "Christmas Movie Challenge",
 		description: "A quiz game where you have to guess the movies title by the image.",
@@ -24,7 +48,7 @@ module.exports = exports = [
 		githubLink: "https://github.com/ryandixon555/avvio-guess-the-movie"
 	},
 	{
-		id: 8,
+		id: 2,
 		client: "Avvio",
 		name: "Superhero Name Generator",
 		description: "A very simple superhero name generator.",
@@ -36,7 +60,7 @@ module.exports = exports = [
 		githubLink: "https://github.com/ryandixon555/avvio-superhero-name-generator/tree/master"
 	},
 	{
-		id: 7,
+		id: 1,
 		client: "Avvio",
 		name: "Wheel of Fortune",
 		description: "A Wheel of Fortune game. 3 spins to earn as many points as possible to win prizes.",
@@ -46,65 +70,5 @@ module.exports = exports = [
 		noImages: 3,
 		playableLink: "https://ryandixon555.github.io/avvio-wheel-of-fortune/public/index.html",
 		githubLink: "https://github.com/ryandixon555/avvio-wheel-of-fortune"
-	},
-	{
-		id: 6,
-		client: "Avvio",
-		name: "Drag And Drop Game",
-		description: "A drag and drop game – match the ‘perfect partnership’ pairs.",
-		tags: ["Drag and drop", "Matching","Game", "Avvio", "Avvio Reply", "Vanilla JS"],
-		notes: [],
-		contentFolder: "avvio drag and drop",
-		noImages: 4,
-		playableLink: "https://ryandixon555.github.io/avvio-wordsearch/public/index.html",
-		githubLink: "https://ryandixon555.github.io/avvio-wordsearch/public/index.html"
-	},
-	{
-		id: 4,
-		client: "Avvio",
-		name: "Wher'es Wally",
-		description: "A ‘Where’s Wally’ style game – find and click on the 15 hidden Easter eggs in the illustration to reveal some of the benefits they have at Sky.",
-		tags: ["Finder", "Illustration","Game", "Avvio", "Avvio Reply", "Vanilla JS"],
-		notes: [],
-		contentFolder: "avvio wheres wally",
-		noImages: 3,
-		playableLink: "https://ryandixon555.github.io/avvio-wordsearch/public/index.html",
-		githubLink: "https://ryandixon555.github.io/avvio-wordsearch/public/index.html"
-	},
-	{
-		id: 3,
-		client: "Avvio",
-		name: "Avvio Claw",
-		description: "A timed reaction speed game – the digital grabber moves left to right, when it looks aligned with one of the icons you need to click the GRAB button to collect it.",
-		tags: ["Claw grabber", "Innovation Month", "Timed", "Game", "Avvio", "Avvio Reply", "Vanilla JS"],
-		notes: [],
-		contentFolder: "avvio claw",
-		noImages: 5,
-		playableLink: "https://ryandixon555.github.io/avvio-wordsearch/public/index.html",
-		githubLink: "https://ryandixon555.github.io/avvio-wordsearch/public/index.html"
-	},
-	{
-		id: 2,
-		client: "Avvio",
-		name: "Space Raiders",
-		description: "A timed reaction speed game – the graphic at the bottom moves from left to right, when it looks aligned with one of the barrier rows you need to click the button to ZAP them. The aim is to zap all the barriers and release the 6 icons at the top.",
-		tags: ["Space Invaders", "Innovation Month", "Timed", "Game", "Avvio", "Avvio Reply", "Vanilla JS"],
-		notes: [],
-		contentFolder: "avvio space raiders",
-		noImages: 6,
-		playableLink: "https://ryandixon555.github.io/avvio-wordsearch/public/index.html",
-		githubLink: "https://ryandixon555.github.io/avvio-wordsearch/public/index.html"
-	},
-	{
-		id: 1,
-		client: "Avvio",
-		name: "Snake",
-		description: "A snake style game where you use the arrows to move the ‘snake’ around the board and catch the icons that pop up.",
-		tags: ["Snake", "Innovation Month", "Game", "Avvio", "Avvio Reply", "Vanilla JS"],
-		notes: [],
-		contentFolder: "avvio snake",
-		noImages: 5,
-		playableLink: "https://ryandixon555.github.io/avvio-wordsearch/public/index.html",
-		githubLink: "https://ryandixon555.github.io/avvio-wordsearch/public/index.html"
 	}
 ];
