744e815ce9a6298bdb0bdda9851077c91ba6dd71
Ryan Dixon <r.dixon@reply.com>
2020-03-18 10:16:10 +0000

    added projects

--- source/js/projects.js
+++ source/js/projects.js
@@ -1,13 +1,31 @@
 module.exports = exports = [
 	{
+		id: 10,
+		client: "Personal Projects",
+		name: "Flappy Bird",
+		description: "A small JS game based on flappy bird",
+		tags: ["Game", "Vanilla JS"],
+		notes: [],
+		playableLink: "https://ryandixon555.github.io/flappy-bird/",
+		githubLink: "https://github.com/ryandixon555/flappy-bird"
+	},
+	{
+		id: 9,
+		client: "Avvio",
+		name: "Racing Game",
+		description: "A game where you have to raqce against the computer",
+		tags: ["Game", "Avvio", "Avvio Reply", "Vanilla JS"],
+		notes: [],
+		playableLink: "https://ryandixon555.github.io/avvio-racing/public/index.html",
+		githubLink: "https://github.com/ryandixon555/avvio-racing"
+	},
+	{
 		id: 8,
 		client: "Personal Projects",
 		name: "Portfolio",
 		description: "My portfolio, made using GatsbyJS",
 		tags: ["ReactJS", "GatsbyJS", "JavaScript", "Styled Components"],
 		notes: [],
-		contentFolder: "portfolio",
-		noImages: 1,
 		playableLink: "https://www.ryandixonportfolio.com/",
 		githubLink: "https://github.com/ryandixon555/personal-website-V3"
 	},
@@ -18,8 +36,6 @@ module.exports = exports = [
 		description: "An app showing an exercise tracker using the MERN stack",
 		tags: ["ReactJS", "MERN", "JavaScript", "API", "Bootstrap", "MongoDB", "React Router", "AxiosJS"],
 		notes: [],
-		contentFolder: "react exercise tracker",
-		noImages: 1,
 		playableLink: "https://5e49823334f94f567773d0db--frosty-goldwasser-5ce2b8.netlify.com/",
 		githubLink: "https://github.com/ryandixon555/react-full-stack-exercise-tracker"
 	},
@@ -30,8 +46,6 @@ module.exports = exports = [
 		description: "An app showing you recipes. Made using ReactJS where is connects to an API. Also includes filtering.",
 		tags: ["ReactJS", "filtering", "JavaScript"],
 		notes: [],
-		contentFolder: "react recipe",
-		noImages: 1,
 		playableLink: "https://react-js-recipe.netlify.com/",
 		githubLink: "https://github.com/ryandixon555/"
 	},
@@ -42,8 +56,6 @@ module.exports = exports = [
 		description: "A To Do list made using ReactJS",
 		tags: ["ReactJS", "JavaScript"],
 		notes: [],
-		contentFolder: "react to do",
-		noImages: 2,
 		playableLink: "https://react-js-to-do.netlify.com/",
 		githubLink: "https://github.com/ryandixon555/react-todo-app"
 	},
@@ -54,8 +66,6 @@ module.exports = exports = [
 		description: "A wordsearch game",
 		tags: ["Wordsearch", "Game", "Avvio", "Avvio Reply", "Handlebars", "Popups", "Vanilla JS"],
 		notes: [],
-		contentFolder: "avvio wordsearch",
-		noImages: 3,
 		playableLink: "https://ryandixon555.github.io/avvio-wordsearch/public/index.html",
 		githubLink: "https://github.com/ryandixon555/avvio-wordsearch/tree/master"
 	},
@@ -66,8 +76,6 @@ module.exports = exports = [
 		description: "A quiz game where you have to guess the movies title by the image.",
 		tags: ["Quiz","Game", "Avvio", "Avvio Reply", "Vanilla JS"],
 		notes: [],
-		contentFolder: "avvio movie challenge",
-		noImages: 3,
 		playableLink: "https://ryandixon555.github.io/avvio-guess-the-movie/public/index.html",
 		githubLink: "https://github.com/ryandixon555/avvio-guess-the-movie"
 	},
@@ -78,8 +86,6 @@ module.exports = exports = [
 		description: "A very simple superhero name generator.",
 		tags: ["Generator","Game", "Avvio", "Avvio Reply", "Vanilla JS", "Popups"],
 		notes: [],
-		contentFolder: "avvio superhero name generator",
-		noImages: 3,
 		playableLink: "https://ryandixon555.github.io/avvio-superhero-name-generator/public/index.html",
 		githubLink: "https://github.com/ryandixon555/avvio-superhero-name-generator/tree/master"
 	},
@@ -90,8 +96,6 @@ module.exports = exports = [
 		description: "A Wheel of Fortune game. 3 spins to earn as many points as possible to win prizes.",
 		tags: ["Game", "Avvio", "Avvio Reply", "Gambling", "Vanilla JS"],
 		notes: [],
-		contentFolder: "avvio wheel of fortune",
-		noImages: 3,
 		playableLink: "https://ryandixon555.github.io/avvio-wheel-of-fortune/public/index.html",
 		githubLink: "https://github.com/ryandixon555/avvio-wheel-of-fortune"
 	}
